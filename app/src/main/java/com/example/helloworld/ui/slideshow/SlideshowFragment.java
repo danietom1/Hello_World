bbb6e08a91291753ed9cb21e017ee6411b5ef2a7
Daniel Nieto <danietom1@poligran.edu.co>
2021-04-23 14:28:22 -0500

    Layout principal v4.2 Cambios en la pantalla Settings

--- app/src/main/java/com/example/helloworld/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/helloworld/ui/slideshow/SlideshowFragment.java
@@ -7,9 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.helloworld.R;
@@ -22,7 +20,7 @@ public class SlideshowFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        View root = inflater.inflate(R.layout.fragment_repository, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
